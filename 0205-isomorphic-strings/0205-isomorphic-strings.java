4208a1c348f195994556a2798dc75d79a9822a58
Siddharth  Nayak <nayaksiddharth318@gmail.com>
2026-04-13 22:38:37 +0530

    Time: 10 ms (68.74%), Space: 44 MB (47.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,41 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+
+     // HashMap to store mapping between characters of s -> t
+     // Example: a -> x
      HashMap <Character , Character> map = new HashMap();
 
+    // Traverse both strings character by character
     for(int i=0 ; i<s.length()  ;i++){
+
+        // Current character from string s
         Character sCh = s.charAt(i);
+
+        // Current character from string t
         Character tCh = t.charAt(i);
+
+        // If character from s already has a mapping
         if(map.containsKey(sCh)){
+
+            // Check if the mapped character is same as current t character
+            // If not same → strings are not isomorphic
             if( map.get(sCh) != tCh) return false;
         }
+
+        // If sCh is not mapped yet but tCh is already mapped
+        // to some other character → invalid mapping
         else if(map.containsValue(tCh)){
              return false;
         }
-    
+
+        // If both characters are new
+        // create a new mapping sCh -> tCh
     else {
         map.put(sCh ,tCh);
       }
     }
+
+        // If no conflicts found, strings are isomorphic
         return true;
     }
 }
